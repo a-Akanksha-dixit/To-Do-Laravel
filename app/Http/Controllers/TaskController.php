7c41b1c1a86b2727e47c97fb50b9bbb83588d212
Akanksha-dixit-106 <akankshadixit106@gmail.com>
2024-04-27 23:52:53 +0530

    added api for new task create

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tasks;
+
+class TaskController extends Controller
+{
+    //
+    public function create(Request $request) {
+        try {
+            $request->validate([
+                'title' => 'required|string|unique:Tasks,title|min:4|max:255',
+                'description' => 'required|string|min:5',
+                'due_date' => 'required|date_format:Y-m-d'
+            ]);
+            $task = new Tasks();
+            $task->title = $request->title;
+            $task->description = $request->description;
+            $task->due_date = $request->due_date;
+            $task->save();
+            return response()->json(['task' => $task], 201);
+        } catch(\Exception $error) {
+            return response()->json(['error' => $error->getMessage()], 500);
+        }
+        die('in create');
+    }
+}
